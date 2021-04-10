535ba8d66c7f8dae411f7479800daa1ebd0ba531
CheaSim <852975133@qq.com>
2021-04-10 16:56:36 +0800

    modify run.sh

--- dialogue_pl/run.sh
+++ dialogue_pl/run.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=2 python main.py --gpus="0," --max_epochs=300  --num_workers=8 \
-    --model_name_or_path ./origin_bert \
+CUDA_VISIBLE_DEVICES=2 python main.py --gpus="0," --max_epochs=20  --num_workers=8 \
+    --model_name_or_path ./bert-base-uncased \
     --accumulate_grad_batches 3 \
     --batch_size 8 \
-    --data_dir dataset/dialogue_8shot \
+    --data_dir dataset/dialogue \
     --check_val_every_n_epoch 3 \
     --model_class BertForSequenceClassification \
     --wandb  \
